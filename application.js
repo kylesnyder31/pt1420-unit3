da7a023915f37738fdf9b56e5986f94cdef7c40b
kylesnyder31 <kylesnyder31@gmail.com>
2015-06-29 22:40:09 -0500

    so lost

--- application.js
+++ application.js
@@ -1,9 +1,37 @@
+var toDoList = [];
+
+function changeDisplay(){
+	var list = document.getElementById("todolist");
+	list.innerHTML = "";
+	for(var i=0; i<toDoList.length; i++){
+	list.innerHTML += '<div class="item">' + toDoList[i] +
+	' (<a href="#" onclick="javascript:removeToDo(' + i +')">x</a>)' +
+	'</div>';
+	}
+}
+	
+function removeToDo(itemToRemove){
+	var newList = [];
+	for(var i=0; i<toDoList.length; i++){
+	 if( i !== itemToRemove) {
+		newList.push(toDoList[i]);
+	  }
+	}
+	toDoList = newList;
+	changeDisplay();	
+}
+
 function addtodolist(){
-	var toDoList = [];
-	var list;
-	list = document.getElementById("todolist");
 	
-	toDoList[0] = "take out the dog."
+	var tmpItem;
+	var newToDo = document.getElementById("todonew");
 	
-	list.innerHTML += '<div class="item">' + toDoList[0] + '</div>';
+	tmpItem = newToDo.value;
+	
+	toDoList.push(tmpItem);
+	
+	changeDisplay();
+	
+	newToDo.value = "";
+	newToDo.select();
 }
